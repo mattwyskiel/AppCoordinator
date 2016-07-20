253a63c3dfddab950ab1a6371a59a6cf6f520ef6
Matthew Wyskiel <mwyskiel@optonline.net>
2016-07-19 23:14:23 -0400

    Initial Commit

---
+++ AppCoordinator/AppCoordinator.h
@@ -0,0 +1,19 @@
+//
+//  AppCoordinator.h
+//  AppCoordinator
+//
+//  Created by Matthew Wyskiel on 7/19/16.
+//  Copyright © 2016 Matthew Wyskiel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppCoordinator.
+FOUNDATION_EXPORT double AppCoordinatorVersionNumber;
+
+//! Project version string for AppCoordinator.
+FOUNDATION_EXPORT const unsigned char AppCoordinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppCoordinator/PublicHeader.h>
+
+
